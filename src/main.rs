43553a8efa9684952e04d3889f039fc2dd1ab8d1
Gota Gando <gota@gando.dev>
2023-10-07 00:11:16 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use iced::{
+    button, Button, Column, Element, Image, Length, Row, Sandbox, Settings, Text,
+    Container
+};
+use iced::alignment::{Horizontal, Vertical};
+
+// Define the application state
+#[derive(Default)]
+struct ImageViewer {
+    image_path: String,
+    load_button_state: button::State,
+}
+
+// Define application messages
+#[derive(Debug, Clone)]
+enum Message {
+    LoadImage,
+}
+
+
+impl Sandbox for ImageViewer {
+    type Message = Message;
+
+    fn new() -> Self {
+        ImageViewer::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("Image Viewer")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::LoadImage => {
+                // Simulate loading an image (replace with actual image loading logic)
+                self.image_path = "sample.jpg".to_string();
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        let image: Element<Message> = if self.image_path.is_empty() {
+            Text::new("No image loaded")
+                .size(30)
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .horizontal_alignment(Horizontal::Center)
+                .vertical_alignment(Vertical::Center)
+                .into()
+        } else {
+            Image::new(&self.image_path)
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .into()
+        };
+
+        let load_button: Element<Message> = Button::new(&mut self.load_button_state, Text::new("Load Image"))
+            .on_press(Message::LoadImage)
+            .into();
+
+        // Create a simple UI layout with a button and the image
+        let content: Element<Message> = Column::new()
+            .push(load_button)
+            .push(Row::new().push(image).spacing(10))
+            .spacing(20)
+            .padding(20)
+            .into();
+
+        // Wrap the content in a container
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
+    }
+}
+
+fn main() -> iced::Result {
+    ImageViewer::run(Settings::default())
+}
