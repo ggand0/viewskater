5d5eb2889f5f34e23775422efdd9a56c414f5637
Gota Gando <gota@gando.dev>
2024-03-09 18:03:59 +0900

    Fix build error for mac/windows

--- src/viewer.rs
+++ src/viewer.rs
@@ -1,5 +1,25 @@
 //! Zoom and pan on an image.
 
+#[cfg(target_os = "linux")]
+mod other_os {
+    pub use iced;
+    pub use iced_aw;
+    pub use iced_widget;
+}
+
+#[cfg(not(target_os = "linux"))]
+mod macos {
+    pub use iced_custom as iced;
+    pub use iced_aw_custom as iced_aw;
+    pub use iced_widget_custom as iced_widget;
+}
+
+#[cfg(target_os = "linux")]
+use other_os::*;
+
+#[cfg(not(target_os = "linux"))]
+use macos::*;
+
 use iced_widget::{
     core::{
         event::{self, Event},
