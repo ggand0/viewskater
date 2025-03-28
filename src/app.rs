5029308ce42e554faef2b103bb1cc7798ef79d87
Gota Gando <gota@gando.dev>
2025-03-28 16:44:32 +0900

    Bump version to 0.2.1

--- src/app.rs
+++ src/app.rs
@@ -947,7 +947,7 @@ impl iced_winit::runtime::Program for DataViewer {
                         style: iced_winit::core::font::Style::Normal,
                     }),
                     column![
-                        text("Version 0.2.0").size(15),
+                        text("Version 0.2.1").size(15),
                         row![
                             text("Author:  ").size(15),
                             text("Gota Gando").size(15)
